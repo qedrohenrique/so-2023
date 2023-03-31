4a567575dc1668ad1b269dadade7e1af7140d34c
qedrohenrique <pedro.005henr@gmail.com>
2023-03-31 02:59:11 -0300

    Handling operators
    This kinda handles the differents operators in one command line.
    It is working well for pipe operator, but not for the others. This might be fixed with a switch-case.

--- shell-1.c
+++ shell-1.c
@@ -4,25 +4,68 @@
 #include <sys/wait.h>
 #include <strings.h>
 
-int get_operator_position(char** argv){
-	int i = 0;
-	while(argv[i]) {
-		if (argv[i][0] == '|' || // Isso inclui o operador "||" 
-			argv[i][0] == '<' || 
-			argv[i][0] == '>' || // Isso inclui o operador ">>"
-			argv[i][0] == '&'	 // Isso inclui o operador "&&"
+#define OPERATOR_OR 0
+#define OPERATOR_AND 1
+#define OPERATOR_PIPE 2
+#define OPERATOR_BACKGROUND 3
+#define OPERATOR_INPUT 4
+#define OPERATOR_OUTPUT_OVERWRITE 5
+#define OPERATOR_OUTPUT_APPEND 6
+
+/*
+	Retorna a posicao do primeiro operador da matriz de argumentos, dada uma posicao inicial.
+*/
+int get_operator_position(int pos, char** argv){
+	while(argv[pos]) {
+		if (argv[pos][0] == '|' || // Isso inclui o operador "||" 
+			argv[pos][0] == '<' || 
+			argv[pos][0] == '>' || // Isso inclui o operador ">>"
+			argv[pos][0] == '&'	 // Isso inclui o operador "&&"
 			){
-			return i;
+			return pos;
 		}
-		i++;
+		pos++;
 	}
 	return -1;
 }
 
-void imprime_argv(char **argv) {
+/*
+	Retorna o numero de operadores na matriz de argumentos.
+*/
+int count_operators(char **argv){
+    int i = 0, count = 0;
+    while (argv[i]){
+        if (argv[i][0] == '|' || // Isso inclui o operador "||" 
+			argv[i][0] == '<' || 
+			argv[i][0] == '>' || // Isso inclui o operador ">>"
+			argv[i][0] == '&'	 // Isso inclui o operador "&&"
+			) count++;
+        i++;
+    }
+    return count;
+}
+
+/*
+	Retorna o tipo de um operador, dada uma posicao.
+*/
+int get_operator_type(int pos, char** argv){
+	if(argv[pos][0] == '|' && argv[pos][1] == '|') return OPERATOR_OR;
+	if(argv[pos][0] == '&' && argv[pos][1] == '&') return OPERATOR_AND;
+	if(argv[pos][0] == '|') return OPERATOR_PIPE;
+	if(argv[pos][0] == '&') return OPERATOR_BACKGROUND;
+	if(argv[pos][0] == '<') return OPERATOR_INPUT;
+	if(argv[pos][0] == '>') return OPERATOR_OUTPUT_OVERWRITE;
+	if(argv[pos][0] == '>' && argv[pos][1] == '>') return OPERATOR_OUTPUT_APPEND;
+	return -1;
+}
+
+/*
+	Imprime a matriz de argumentos.
+*/
+void imprime_argv(char **argv, int argc) {
 	int i = 0;
 	if (argv[i]) printf("===\n");
-	while(argv[i]) {
+	while(i != argc) {
 		printf("%d: '%s'\n", i, argv[i]);
 		i++;
 	}
@@ -31,27 +74,63 @@ void imprime_argv(char **argv) {
 int main(int argc, char** argv){
 
 	if(argc==1){
-		printf("Uso: <comando> <arg1> ... < argn>.\n");
+		printf("Uso: <comando> <arg1> ... <argn>.\n");
 		return 0;
 	}
 
-	int operator_position;
-	operator_position = get_operator_position(argv);
+	char** cmd = &argv[1];
 
-	char **cmd1, **cmd2;
-	cmd1 = &argv[1];
-	cmd2 = &argv[operator_position+1];
-
-	// Comando unico
-	if(operator_position == -1){
-		execvp(cmd1[0], cmd1);
+	// Comando simples (sem nenhum operador)
+	if(get_operator_position(0, argv) == -1){
+		execvp(cmd[0], cmd);
 	}
 
-	int fd1[2], fd2[2];
-	if(pipe(fd1) == -1 || pipe(fd2) == -1){
-		perror("pipe()");
-		return -1;
-	}
+	// Comando com operadores
+	else{
+
+		int fd[2], command_pos = 1, operator_pos;
+		int aux = STDIN_FILENO;
+		int num_op = count_operators(argv);
+
+		for(int i = 0; i < num_op+1; i++){
+			operator_pos = get_operator_position(command_pos, argv);
+			cmd = &argv[command_pos];
+
+			if (operator_pos != -1) {
+				char* operator = argv[operator_pos];
+				cmd[operator_pos - command_pos] = NULL;
+			}
+			
+			//printf("%s - %d com %d numero de argumentos.\n", argv[command_pos], command_pos, operator_pos-command_pos-1);
+
+			if (pipe(fd) < 0){
+	            perror("pipe()");
+	            return -1;
+	        }
+
+			pid_t p_id = fork();
+			
+			if (p_id == 0){    
+				// printf("Filho: Processo (%d) - Comando (%s) %s\n", getpid(), cmd[0], cmd[1]);
+	            close(fd[0]);            
+	            dup2(aux, STDIN_FILENO); 
+	            if (i < num_op) dup2(fd[1], STDOUT_FILENO); 
+	            execvp(cmd[0], cmd);
+	            return 0;
+	        }
+	        else if (p_id > 0){ 
+				// printf("Pai: Processo (%d) - Comando (%s) %s\n", getpid(), cmd[0], cmd[1]);
+	            aux = fd[0];
+	            close(fd[1]);
+	            waitpid(p_id, NULL, 0);
+	        }else{
+	            perror("fork()");
+	            return -1;
+	        }
+
+	        command_pos = operator_pos + 1;
+	    }
+	}	
 
 	return 0;
 }
