8a8a96d233612717d071e3abe655b8e95000e7a2
qedrohenrique <pedro.005henr@gmail.com>
2023-03-30 21:56:50 -0300

    added commands and file descriptors.

--- shell-1.c
+++ shell-1.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <strings.h>
 
 int get_operator_position(char** argv){
 	int i = 0;
@@ -15,6 +19,15 @@ int get_operator_position(char** argv){
 	return -1;
 }
 
+void imprime_argv(char **argv) {
+	int i = 0;
+	if (argv[i]) printf("===\n");
+	while(argv[i]) {
+		printf("%d: '%s'\n", i, argv[i]);
+		i++;
+	}
+}
+
 int main(int argc, char** argv){
 
 	if(argc==1){
@@ -23,7 +36,22 @@ int main(int argc, char** argv){
 	}
 
 	int operator_position;
-	operator_position = get_operator_position(argv);	
+	operator_position = get_operator_position(argv);
+
+	char **cmd1, **cmd2;
+	cmd1 = &argv[1];
+	cmd2 = &argv[operator_position+1];
+
+	// Comando unico
+	if(operator_position == -1){
+		execvp(cmd1[0], cmd1);
+	}
+
+	int fd1[2], fd2[2];
+	if(pipe(fd1) == -1 || pipe(fd2) == -1){
+		perror("pipe()");
+		return -1;
+	}
 
 	return 0;
 }
