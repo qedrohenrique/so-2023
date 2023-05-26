e604e277182a4855398025ee454df997d746dbaf
qedrohenrique <pedro.005henr@gmail.com>
2023-05-26 14:06:44 -0300

    nur algorithm

--- trabalho 03/main.c
+++ trabalho 03/main.c
@@ -11,8 +11,8 @@
 
 /*
 		TODO:
-	Trap NUR
-
+	O bit de modificado esta sendo setado corretamente?
+	Adicionar media e dp aos numeros sorteados.
 */
 
 void fill_memory(List* mem, int mem_size, int page_id, int start){
@@ -90,7 +90,100 @@ Page trap_second_chance(List *mr, List* ms, int id){
 }
 
 Page trap_nur(List *mr, List* ms, int id){
-	
+	Page ms_page = list_search_page(ms , id);
+	Page aux_page = create_page(-1);
+	int i;
+	// CLASSE 0
+
+	Page mr_page = mr->head;
+	List* aux_list = list_init();
+	i = 0;
+	while(i++ < mr->length){
+		if(mr_page->ref == 0 && mr_page->mod == 0){
+			list_insert(aux_list, create_page(mr_page->id));
+		}
+		mr_page = mr_page->next;
+	}
+
+	if(aux_list->length != 0){	
+		Page page = aux_list->head;
+		srand(0);
+		int num = rand() % aux_list->length;
+		for(int i = 0; i < num-1; i++) page = page->next;
+		mr_page = list_search_page(mr, page->id);
+
+		copy_page(mr_page, aux_page);
+		copy_page(ms_page, mr_page);
+		copy_page(aux_page, ms_page);
+		return mr_page;
+	}
+
+	// CLASSE 1
+
+	mr_page = mr->head;
+	aux_list = list_init();
+	i = 0;
+	while(mr_page != NULL){
+		if(mr_page->ref == 1 && mr_page->mod == 0){
+			list_insert(aux_list, create_page(mr_page->id));
+		}
+		mr_page = mr_page->next;
+	}
+
+	if(aux_list->length != 0){
+		Page page = aux_list->head;
+		srand(0);
+		int num = rand() % aux_list->length;
+		for(int i = 0; i < num; i++) page = page->next;
+
+		mr_page = list_search_page(mr, page->id);
+
+		copy_page(mr_page, aux_page);
+		copy_page(ms_page, mr_page);
+		copy_page(aux_page, ms_page);
+		return mr_page;
+	}
+
+
+	// CLASSE 2
+
+	mr_page = mr->head;
+	aux_list = list_init();
+	i = 0;
+	while(mr_page != NULL){
+		if(mr_page->ref == 0 && mr_page->mod == 1){
+			list_insert(aux_list, create_page(mr_page->id));
+		}
+		mr_page = mr_page->next;
+	}
+
+	if(aux_list->length != 0){
+		Page page = aux_list->head;
+		srand(0);
+		int num = rand() % aux_list->length;
+		for(int i = 0; i < num; i++) page = page->next;
+
+		mr_page = list_search_page(mr, page->id);
+
+		copy_page(mr_page, aux_page);
+		copy_page(ms_page, mr_page);
+		copy_page(aux_page, ms_page);
+
+		return mr_page;
+	}
+
+
+	// CLASSE 3
+
+	mr_page = mr->head;
+
+		srand(0);
+		int num = rand() % mr->length;
+		for(int i = 0; i < num; i++) mr_page = mr_page->next;
+		copy_page(mr_page, aux_page);
+		copy_page(ms_page, mr_page);
+		copy_page(aux_page, ms_page);	
+		return mr_page;	
 }
 
 int main(int argc, char** argv){
@@ -129,15 +222,16 @@ int main(int argc, char** argv){
 
 		for(i = 0; i < MAX_REP; i++){
 				int pag_virutal = get_random_page_id(i, exp_num, std_dev, mean);
-				printf("%d\n", pag_virutal);
+				
 				Page pag_real = list_search_page(MR, pag_virutal);
 
 				if(pag_real == NULL){
-					pag_real = trap_second_chance(MR, MS, pag_virutal);
+					pag_real = trap_nur(MR, MS, pag_virutal);
 					page_miss++;
 				}
 
 				list_ref_page(MR, pag_real->id);
+				if(i%10==0)list_mod_page(MR, pag_real->id);
 		}
 
 		printf("Page miss: %d\n", page_miss);
